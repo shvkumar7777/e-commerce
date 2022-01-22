cba86300fe93a05d5c045d2642986a7c4c0c9554
shvkumar7777 <43500125+shvkumar7777@users.noreply.github.com>
2022-01-22 22:42:23 +0530

    added navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/Navbar/NavBar';
 import Products from './components/Products/Products';
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Products/>
     </div>
   );
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AppBar,Toolbar,IconButton,Badge,Menu,MenuItem,Typography } from '@mui/material';
+import { ShoppingCart } from '@material-ui/icons';
+
+import useStyles from './Styles';
+import logo from '../../Assets/commerce.png'
+    
+const NavBar = () => {
+    const styles = useStyles();
+    return (
+        <>
+            <AppBar className={styles.appBar} position='fixed' color='inherit'>
+                <Toolbar>
+                    <Typography variant='h6' className={styles.title} color='inherit'>
+                        <img src={logo} alt='commerce.js' height='25px' className={styles.img} /> 
+                        E-Commerce
+                    </Typography>
+                    <div className={styles.grow} />   
+                    <div className={styles.button}>
+                        <IconButton aria-label='show cart items' color='inherit'>
+                            <Badge badgeContent={2} color='secondary'>
+                                <ShoppingCart></ShoppingCart>
+                            </Badge>
+                        </IconButton>
+                    </div>    
+                </Toolbar>
+            </AppBar>
+
+        </>
+    );
+};
+
+export default NavBar;
